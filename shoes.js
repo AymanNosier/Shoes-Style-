83ff31dacae25300c5bb01b4ca2cc7a9fcd06c9c
Ayman Nosier <119597392+AymanNosier@users.noreply.github.com>
2022-12-04 14:51:43 -0800

    Add files via upload

---
+++ shoes.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+$(".img5").hover(function(){
+    $(".img1").hide(1);
+    $(".img2").show(1);
+    $(".img3").hide(1);
+    $(".img4").hide(1);
+})
+$(".img6").hover(function(){
+    $(".img1").hide(1);
+    $(".img2").hide(1);
+    $(".img3").show(1);
+    $(".img4").hide(1);
+})
+$(".img7").hover(function(){
+    $(".img1").hide(1);
+    $(".img2").hide(1);
+    $(".img3").hide(1);
+    $(".img4").show(1);
+})
+})
